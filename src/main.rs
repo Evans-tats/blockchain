3ce4b90de10623390356154ba7f91ee9e0dc1727
Evans <evanstats808@gmail.com>
2024-01-16 22:53:51 +0300

    implemeted block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+pub struct Block {
+    timestamp: u128,
+    transactions: String,
+    previous_block_hash: String,
+    hash: String,
+    height: usize,
+    nonce: String,
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
