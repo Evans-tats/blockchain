624ef4c4ceeb1b509aadfee4d34328f5fbcea0ab
Evans <evanstats808@gmail.com>
2024-01-22 23:00:26 +0300

    day 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::alloc::System;
+
 pub struct Block {
     timestamp: u128,
     transactions: String,
@@ -7,7 +9,24 @@ pub struct Block {
     nonce: String,
 }
 
+pub struct blockchain {
+    blocks :Vec<Block>
+}
 
+impl Block {
+    pub fn new_block(prev_block_hash: String, data: String, height: usize) -> Result<Block> {
+        let timestamp = System::now()
+            .duration_since(System::UNIX_EPOCH)?.as_millis();
+        let mut block = Block {
+            timestamp: timestamp,
+            transactions: Vec::new(),
+            previous_block_hash,
+            hash: String::new(),
+            height,
+            nonce: 0,
+        };
+    }
+}
 
 fn main() {
     println!("Hello, world!");
